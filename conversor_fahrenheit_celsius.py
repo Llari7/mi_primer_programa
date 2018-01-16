24a8b056074adb81982efa6970a2b57b38bcf180
Jerik <Llari_rq7>
2018-01-16 12:59:07 +0100

    Conversor Fahrenheit -> Celsius, ejercicio 3 de tarea para casa del curso de Python de NateAcademy

---
+++ conversor_fahrenheit_celsius.py
@@ -0,0 +1,3 @@
+grados_fahrenheit = float(input("Introduzca la cantidad de grados Fahrenheit que quiera convertir a grados Centígrados: "))
+grados_celsius = (grados_fahrenheit - 32) / 1.8
+print("{} ºF --> {} ºC".format(grados_fahrenheit, grados_celsius))
