df198ff0958b34d3c929b0d404e766c3ea6c2d3d
Llari7 <llarirq7@gmail.com>
2018-01-22 11:09:44 +0100

    Programa sencillo que multiplica todos los números de una lista

---
+++ multiplicador_de_lista.py
@@ -0,0 +1,7 @@
+lista_numeros = [1, 2, 4, 6]
+resultado = 1
+
+for num in lista_numeros:
+    resultado *= num
+
+print("El resultado de multiplicar todos los números de la lista es: {}".format(resultado))
